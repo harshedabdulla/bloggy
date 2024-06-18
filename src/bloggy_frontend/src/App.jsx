c44dbbf3b8c97f45ec71de0a42ca29983b573179
harshedabdulla <harshed2004@gmail.com>
2024-06-18 21:26:50 +0530

    added protected route

--- src/bloggy_frontend/src/App.jsx
+++ src/bloggy_frontend/src/App.jsx
@@ -3,15 +3,29 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import NewPost from './pages/NewPost'
 import Posts from './pages/Posts'
 import UserLogin from './pages/UserLogin'
-
+import ProtectedRoute from './components/ProtectedRoute'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<UserLogin />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/post/new" element={<NewPost />} />
+        <Route
+          path="/posts"
+          element={
+            <ProtectedRoute>
+              <Posts />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/post/new"
+          element={
+            <ProtectedRoute>
+              <NewPost />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
